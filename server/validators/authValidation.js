47273055ee37453eb2ba7e634036deb29894f217
ayush0816 <75437420+ayush0816@users.noreply.github.com>
2023-04-05 03:04:08 +0530

    updated authValidation

--- server/validators/authValidation.js
+++ server/validators/authValidation.js
@@ -1,21 +1,20 @@
-const joi=require('joi');
+const joi = require("joi");
 const ValidateSignup = (userData) => {
-    const Scheme = joi.object({
-        name: joi.string().min(3).max(20),
-        username: joi.string().email(),
-        password: joi.string().required().min(5)
-       
-    })
+  const Schema = joi.object({
+    name: joi.string().min(3).max(20),
+    username: joi.string().email(),
+    password: joi.string().required().min(5),
+  });
 
-    return Scheme.validateAsync(userData);
-}
+  return Schema.validateAsync(userData);
+};
 
 const ValidateSignin = (userData) => {
-    const Scheme = joi.object({
-        username: joi.string().email(),
-        password: joi.string().required().min(5),
-    })
+  const Schema = joi.object({
+    username: joi.string().email(),
+    password: joi.string().required().min(5),
+  });
 
-    return Scheme.validateAsync(userData);
-}
-module.exports={ValidateSignin,ValidateSignup};
+  return Schema.validateAsync(userData);
+};
+module.exports = { ValidateSignin, ValidateSignup };
