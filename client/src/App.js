50ea5822a0e84a8033217b5557ccece63fce7406
ayush0816 <75437420+ayush0816@users.noreply.github.com>
2023-04-05 22:11:41 +0530

    login page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { StyledEngineProvider } from "@mui/material/styles";
+import Login from "./components/Login";
 
 function App() {
-  return <div className="App">detoxx</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Login />
+    </div>
+  );
 }
 
 export default App;
